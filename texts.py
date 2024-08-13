f643c3cf8e55da0b22a441cb83421dd5b7332577
xDilettante <email.b.o.x@mail.ru>
2024-08-14 01:27:20 +0300

    Edit

--- texts.py
+++ texts.py
@@ -1,6 +1,5 @@
 from PySide6.QtCore import QCoreApplication
 
-# Если текст не будет совпадать с текстом из файлов .ts тега <source> то локализация работать не будет.
 class Texts:
     @property
     def main_window_title(self):
